52cb39beb4cb73a12b321fc9a85e3abc61eedfa1
Favour Njoku <favour.njoku@icloud.com>
2025-03-24 23:18:10 +0100

    feat: listing all data types and sizes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <iomanip>
 
 #ifdef __APPLE__
 #include <sys/types.h>
@@ -10,6 +11,27 @@
 #include <windows.h>
 typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
 #endif
+
+void print_data_types() {
+    std::cout << "\nData Type Sizes:" << std::endl;
+    std::cout << std::setw(20) << std::left << "Data Type" << std::setw(10) << "Size (bytes)" << std::endl;
+    std::cout << std::string(32, '-') << std::endl;
+    
+    std::cout << std::setw(20) << std::left << "bool" << std::setw(10) << sizeof(bool) << std::endl;
+    std::cout << std::setw(20) << std::left << "char" << std::setw(10) << sizeof(char) << std::endl;
+    std::cout << std::setw(20) << std::left << "short" << std::setw(10) << sizeof(short) << std::endl;
+    std::cout << std::setw(20) << std::left << "int" << std::setw(10) << sizeof(int) << std::endl;
+    std::cout << std::setw(20) << std::left << "long" << std::setw(10) << sizeof(long) << std::endl;
+    std::cout << std::setw(20) << std::left << "long long" << std::setw(10) << sizeof(long long) << std::endl;
+    std::cout << std::setw(20) << std::left << "float" << std::setw(10) << sizeof(float) << std::endl;
+    std::cout << std::setw(20) << std::left << "double" << std::setw(10) << sizeof(double) << std::endl;
+    std::cout << std::setw(20) << std::left << "long double" << std::setw(10) << sizeof(long double) << std::endl;
+    std::cout << std::setw(20) << std::left << "void*" << std::setw(10) << sizeof(void*) << std::endl;
+    std::cout << std::setw(20) << std::left << "size_t" << std::setw(10) << sizeof(size_t) << std::endl;
+    std::cout << std::setw(20) << std::left << "wchar_t" << std::setw(10) << sizeof(wchar_t) << std::endl;
+    std::cout << std::setw(20) << std::left << "char16_t" << std::setw(10) << sizeof(char16_t) << std::endl;
+    std::cout << std::setw(20) << std::left << "char32_t" << std::setw(10) << sizeof(char32_t) << std::endl;
+}
 
 bool is_64bit() {
   #if defined(__APPLE__)
@@ -46,5 +68,7 @@ int main() {
     std::cout << "You are on a 32-bit" << device_type << std::endl;
   }
   delete[] device_type;
+  
+  print_data_types();
   return 0;
 }
